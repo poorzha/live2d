b6c69bbe8d9e0be5f3a935c329e5be60441ba58a
zha <495545100@qq.com>
2023-02-04 01:47:14 +0800

    C

--- live2dnew/left/autoload.js
+++ live2dnew/left/autoload.js
@@ -1,7 +1,7 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/poorzha/live2d/live2dnew/left/";
+//const live2d_path = "https://cdn.jsdelivr.net/gh/poorzha/live2d/live2dnew/left/";
+const live2d_path = "/live2d/live2dnew/left/";
 
-//const live2d_path = "/live2d-widget/";
 //const live2d_path="https://api.itggg.cn/live2dnew/left/";
 //const live2d_path = "https://cdn.jsdelivr.net/gh/poorzha/live2d-widget/";
 
@@ -44,12 +44,3 @@ if (screen.width >= 768) {
 		});
 	});
 }
-
-
-
-
-<<<<<<< HEAD
-// function loadExternalResource(url,type){return new Promise((resolve,reject)=>{let tag;if(type==="css"){tag=document.createElement("link");tag.rel="stylesheet";tag.href=url}else if(type==="js"){tag=document.createElement("script");tag.src=url}if(tag){tag.onload=()=>resolve(url);tag.onerror=()=>reject(url);document.head.appendChild(tag)}})}if(screen.width>=768){Promise.all([loadExternalResource(live2d_path+"waifu.min.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.min.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",apiPath:"https://api.itggg.cn/live2d_api/",})})}
-=======
-// function loadExternalResource(url,type){return new Promise((resolve,reject)=>{let tag;if(type==="css"){tag=document.createElement("link");tag.rel="stylesheet";tag.href=url}else if(type==="js"){tag=document.createElement("script");tag.src=url}if(tag){tag.onload=()=>resolve(url);tag.onerror=()=>reject(url);document.head.appendChild(tag)}})}if(screen.width>=768){Promise.all([loadExternalResource(live2d_path+"waifu.min.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.min.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",apiPath:"https://api.itggg.cn/live2d_api/",})})}
->>>>>>> 2fe7b6b2898b1b52b05d895ab3ef6e99f84dc618
